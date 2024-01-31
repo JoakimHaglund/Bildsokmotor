d9a520c56f3b517ff856d1aa4700c8562690e90a
s1ggelin <80677519+s1ggelin@users.noreply.github.com>
2024-01-31 09:53:16 +0100

    Co-authored-by: JoakimHaglund <joakimhaglund@outlook.com>

--- scripts/main.js
+++ scripts/main.js
@@ -14,5 +14,18 @@ function formatSearchTerms(){
 
 }
 function test(){
-    alert("Wel, Hello There!")
+    alert("Well, Hello There!")
 }
+
+let form = document.getElementById('searchForm');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let searchTerm = document.getElementById('searchTerm');
+    let colorLimit = document.getElementById('colors');
+    for(let k in myTest){
+        let p = document.querySelector('p');
+        p.textContent += k.toString();
+    }
+    console.log(myTest);
+    form.reset();
+});
