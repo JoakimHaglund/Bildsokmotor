8627c2f85682e3fd7d0f8b8ae10fadc1789312fe
JoakimHaglund <joakimhaglund@outlook.com>
2024-01-31 15:29:17 +0100

    created function to read data from form and started work on fromatting api url
    Co-authored-by: Andreas Siggelin <andreassiggelin@gmail.com>

--- scripts/main.js
+++ scripts/main.js
@@ -5,27 +5,34 @@ async function getResults(searchTerm) {
     let response = await fetch(url + apikey + searchTerm);
     let json = await response.json();
     return json;
-    /*let temperature = json.current.temperature_2m;
+    let temperature = json.current.temperature_2m;
     let p = document.querySelector('p');
-    p.textContent = 'Current temperature in Gothenburg: ' + temperature;*/
+    p.textContent = 'Current temperature in Gothenburg: ' + temperature;
+};
+function buildApiCall(term, color){
+    return `${url}?key=${apiKey}&q=${term}&colors=${color}`;
 }
-function formatSearchTerms(){
-    let str = '?';
-
-}
-function test(){
-    alert("Well, Hello There!")
-}
-
-let form = document.getElementById('searchForm');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let searchTerm = document.getElementById('searchTerm');
-    let colorLimit = document.getElementById('colors');
-    for(let k in myTest){
-        let p = document.querySelector('p');
-        p.textContent += k.toString();
+function formatSearchTerm(string){
+    let words = string.split(" ");
+    console.log(words);
+    let output = "";
+    for(let word in words) {
+        output += word + "+";
     }
-    console.log(myTest);
-    form.reset();
-});
+    console.log(output);
+};
+
+function onFormSubmit(event){
+    event.preventDefault();
+    const data = new FormData(event.target);
+    let searchTerm = data.get("searchTerm");
+	let searchColor = data.get("searchColor");
+    console.log(`searchTerm: ${searchTerm}, searchColor: ${searchColor}`);
+    /*gör allt */
+    searchTerm = formatSearchTerm(searchTerm);
+    let url = buildApiCall(searchTerm, searchColor);
+    console.log(url);
+}
+
+const form = document.getElementById('searchForm');
+form.addEventListener("submit", onFormSubmit);
